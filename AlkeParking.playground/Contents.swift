7661730cf075b3fe0ca6c582c1b3f06850dd54a1
LiliCa <40767022+lilica-pt@users.noreply.github.com>
2022-08-30 11:57:54 -0300

    FinalResult ParkingLot

--- AlkeParking.playground/Contents.swift
+++ AlkeParking.playground/Contents.swift
@@ -4,13 +4,12 @@
 
 import Foundation
 
+// MARK: 2
 enum VehicleType {
-    case car
-    case moto
-    case miniBus
-    case bus
+    case car, moto, miniBus, bus
 }
 
+// MARK: 1
 protocol Parkable {
     var plate: String { get set }
     var type: VehicleType { get }
@@ -23,18 +22,15 @@ struct Parking {
     let full: Int = 20
     var aviability = (0, 0)
     
+    // MARK: 5
     mutating func checkInVehicle(_ vehicle: Vehicle, onFinish: (Bool) -> Void) {
-        // Insert vehicle here
-       // let approved = vehicles.count < full
-        guard vehicles.count < full else {
-            print("Sorry, the check-in failed")
-            print("the parking is full")
+        guard vehicles.count <= full else {
+            print("Sorry, the check-in failed due to vacancy")
             return onFinish(false)
         }
         for car in vehicles {
             if car.plate == vehicle.plate {
-                print("Sorry, the check-in failed")
-                print("the vehicle \(vehicle.plate) is already checked")
+                print("Sorry, the check-in failed, \(vehicle.plate) already in")
                 return onFinish(false)
             }
         }
@@ -43,43 +39,46 @@ struct Parking {
         return onFinish(true)
     }
     
-    mutating func calculateFee(_ type: VehicleType, _ parkedTime: Int?, hasDiscountCard: String?)->Int{
+    // MARK: 8
+    mutating func calculateFee(_ type: VehicleType, _ parkedTime: Int?, hasDiscountCard: String?) -> Int {
         var fee: Int
         switch type {
-        case .bus:
-            fee = 30
-        case .miniBus:
-            fee = 25
-        case .moto:
-            fee = 15
         case .car:
             fee = 20
+        case .moto:
+            fee = 15
+        case .miniBus:
+            fee = 25
+        case .bus:
+            fee = 30
         }
         
-        guard let parkedTime = parkedTime else { return 0}
+        guard let parkedTime = parkedTime else { return 0 }
         
         if parkedTime > 120 {
-            let coupleHours = parkedTime-120
-            let checkRound = coupleHours / 15
+            let coupleHours = parkedTime - 120
+            let makeRound = coupleHours / 15
             if coupleHours % 15 != 0 {
-                fee = fee + ((checkRound + 1)*5)
+                fee = fee + ((makeRound + 1) * 5)
             } else {
-                fee = fee + (checkRound*5)
+                fee = fee + (makeRound * 5)
             }
         }
         
-        guard let hasDiscountCard = hasDiscountCard else {return fee}
+        // MARK: 9
+        guard let hasDiscountCard = hasDiscountCard else { return fee }
         let discountFee = Int(Double(fee) * 0.85)
-        
         return discountFee
     }
     
-    mutating func checkOutVehicle(plate: String, onSuccess: (Int) -> Void, onError: () -> Void) {
+    // MARK: 7
+    mutating func checkOutVehicle(plate: String, onSucess: (Int) -> Void, onError: () ->  Void){
         for vehicle in vehicles {
             var variableVehicle = vehicle
             if variableVehicle.plate == plate {
                 let fee = calculateFee(variableVehicle.type, variableVehicle.parkedTime, hasDiscountCard: variableVehicle.discountCard)
-                onSuccess(fee)
+                // MARK: 10
+                onSucess(fee)
                 vehicles.remove(vehicle)
                 aviability.0 += 1
                 aviability.1 += fee
@@ -89,85 +88,82 @@ struct Parking {
         }
     }
     
+    // MARK: 11
     func showEarnings(){
-        print("\(aviability.0) vehicles have checked out and have earnings of $\(aviability.1)")
+        print("\(aviability.0) vehicles have checked out and our earnings are \(aviability.1)")
     }
     
+    // MARK: 12
     func listVehicles(){
         for vehicle in vehicles {
             print("plate: \(vehicle.plate)")
         }
     }
-    
 }
 
 struct Vehicle: Hashable, Parkable {
     var plate: String
+    // MARK: 2
     let type: VehicleType
     var checkInTime: Date
     var discountCard: String?
-
     
+    // MARK: 3
     lazy var parkedTime: Int? = {
         let mins = Calendar.current.dateComponents([.minute], from: self.checkInTime, to: Date()).minute ?? 0
         return mins
     }()
     
-    
-    func hash(into hasher: inout Hasher) {
+    func hash(into hasher: inout Hasher){
         hasher.combine(plate)
     }
-
-    static func ==(lhs: Vehicle, rhs: Vehicle)-> Bool {
+    
+    static func ==(lhs: Vehicle, rhs: Vehicle) -> Bool {
         return lhs.plate == rhs.plate
     }
-    
-    
-    
 }
 
+// MARK: 4
 var alkeParking = Parking()
 
-
-let vehicle1 = Vehicle(plate: "1", type: VehicleType.car, checkInTime: Date(), discountCard: "DISCOUNT_CARD_001")
-let vehicle2 = Vehicle(plate: "2", type: VehicleType.moto, checkInTime: Date(), discountCard: nil)
-let vehicle3 = Vehicle(plate: "3", type: VehicleType.miniBus, checkInTime: Date(), discountCard: nil)
-let vehicle4 = Vehicle(plate: "4", type: VehicleType.bus, checkInTime: Date(), discountCard: "DISCOUNT_CARD_002")
-let vehicle5 = Vehicle(plate: "4", type: VehicleType.car, checkInTime: Date(), discountCard: "DISCOUNT_CARD_003")
-let vehicle6 = Vehicle(plate: "6", type: VehicleType.moto, checkInTime: Date(), discountCard: "DISCOUNT_CARD_004")
-let vehicle7 = Vehicle(plate: "7", type: VehicleType.miniBus, checkInTime: Date(), discountCard: nil)
-let vehicle8 = Vehicle(plate: "8", type: VehicleType.bus, checkInTime: Date(), discountCard: "DISCOUNT_CARD_005")
-let vehicle9 = Vehicle(plate: "9", type: VehicleType.car, checkInTime: Date(), discountCard: nil)
-let vehicle10 = Vehicle(plate: "10", type: VehicleType.moto, checkInTime: Date(), discountCard: nil)
-let vehicle11 = Vehicle(plate: "11", type: VehicleType.miniBus, checkInTime: Date(), discountCard: nil)
-let vehicle12 = Vehicle(plate: "12", type: VehicleType.bus, checkInTime: Date(), discountCard: "DISCOUNT_CARD_006")
-let vehicle13 = Vehicle(plate: "13", type: VehicleType.car, checkInTime: Date(), discountCard: "DISCOUNT_CARD_007")
-let vehicle14 = Vehicle(plate: "14", type: VehicleType.moto, checkInTime: Date(), discountCard: nil)
-let vehicle15 = Vehicle(plate: "15", type: VehicleType.miniBus, checkInTime: Date(), discountCard: nil)
-let vehicle16 = Vehicle(plate: "16", type: VehicleType.car, checkInTime: Date(), discountCard: "DISCOUNT_CARD_001")
-let vehicle17 = Vehicle(plate: "17", type: VehicleType.car, checkInTime: Date(), discountCard: "DISCOUNT_CARD_001")
-let vehicle18 = Vehicle(plate: "18", type: VehicleType.moto, checkInTime: Date(), discountCard: nil)
-let vehicle19 = Vehicle(plate: "19", type: VehicleType.bus, checkInTime: Date(), discountCard: "teste")
-let vehicle20 = Vehicle(plate: "20", type: VehicleType.moto, checkInTime: Date(), discountCard: nil)
-let vehicle21 = Vehicle(plate: "21", type: VehicleType.moto, checkInTime: Date(), discountCard: nil)
-let vehicle22 = Vehicle(plate: "22", type: VehicleType.moto, checkInTime: Date(), discountCard: nil)
+// MARK: 6
+let vehicle1 = Vehicle(plate: "AA111AA", type:VehicleType.car, checkInTime: Date(), discountCard:"DISCOUNT_CARD_001")
+let vehicle2 = Vehicle(plate: "B222BBB", type:VehicleType.moto, checkInTime: Date(), discountCard: nil)
+let vehicle3 = Vehicle(plate: "CC333CC", type:VehicleType.miniBus, checkInTime: Date(), discountCard:nil)
+let vehicle4 = Vehicle(plate: "DD444DD", type:VehicleType.bus, checkInTime: Date(), discountCard:"DISCOUNT_CARD_002")
+let vehicle5 = Vehicle(plate: "AA111BB", type:VehicleType.car, checkInTime: Date(), discountCard:"DISCOUNT_CARD_003")
+let vehicle6 = Vehicle(plate: "B222CCC", type:VehicleType.moto, checkInTime: Date(), discountCard:"DISCOUNT_CARD_004")
+let vehicle7 = Vehicle(plate: "CC333CC", type:VehicleType.miniBus, checkInTime: Date(), discountCard:nil)
+let vehicle8 = Vehicle(plate: "DD444EE", type:VehicleType.bus, checkInTime: Date(), discountCard:"DISCOUNT_CARD_005")
+let vehicle9 = Vehicle(plate: "AA111CC", type:VehicleType.car, checkInTime: Date(), discountCard: nil)
+let vehicle10 = Vehicle(plate: "B222DDD", type:VehicleType.moto, checkInTime: Date(), discountCard: nil)
+let vehicle11 = Vehicle(plate: "CC333EE", type:VehicleType.miniBus, checkInTime: Date(), discountCard:nil)
+let vehicle12 = Vehicle(plate: "DD444GG", type:VehicleType.bus, checkInTime: Date(), discountCard:"DISCOUNT_CARD_006")
+let vehicle13 = Vehicle(plate: "AA111DD", type:VehicleType.car, checkInTime: Date(), discountCard:"DISCOUNT_CARD_007")
+let vehicle14 = Vehicle(plate: "B222EEE", type:VehicleType.moto, checkInTime: Date(), discountCard: nil)
+let vehicle15 = Vehicle(plate: "CC333FF", type:VehicleType.miniBus, checkInTime: Date(), discountCard:nil)
+let vehicle16 = Vehicle(plate: "XA111AA", type: VehicleType.car, checkInTime: Date(), discountCard: "DISCOUNT_CARD_001")
+let vehicle17 = Vehicle(plate: "AA11XAA", type: VehicleType.car, checkInTime: Date(), discountCard: "DISCOUNT_CARD_001")
+let vehicle18 = Vehicle(plate: "B222BBB", type: VehicleType.moto, checkInTime: Date(), discountCard: nil)
+let vehicle19 = Vehicle(plate: "B222BCB", type: VehicleType.moto, checkInTime: Date(), discountCard: nil)
+let vehicle20 = Vehicle(plate: "B222DBB", type: VehicleType.moto, checkInTime: Date(), discountCard: nil)
+let vehicle21 = Vehicle(plate: "B222XBB", type: VehicleType.moto, checkInTime: Date(), discountCard: nil)
+let vehicle22 = Vehicle(plate: "B222XBX", type: VehicleType.moto, checkInTime: Date(), discountCard: nil)
 
 
 var myVehicles = [vehicle1, vehicle2, vehicle3, vehicle4, vehicle5, vehicle6, vehicle7, vehicle8, vehicle9, vehicle10, vehicle11, vehicle12, vehicle13, vehicle14, vehicle15, vehicle16, vehicle17, vehicle18, vehicle19, vehicle20, vehicle21, vehicle22]
-
 
 for myVehicle in myVehicles {
     alkeParking.checkInVehicle(myVehicle) { _ in }
 }
 
-alkeParking.checkOutVehicle(plate: "19") { fee in
+alkeParking.checkOutVehicle(plate: "B222EEE") { fee in
     print("⚠️⚠️⚠️⚠️ FEE: \(fee)")
 } onError: {}
 
-alkeParking.checkOutVehicle(plate: "1") { fee in
+alkeParking.checkOutVehicle(plate: "CC333FF") { fee in
     print("⚠️⚠️⚠️⚠️ FEE: \(fee)")
 } onError: {}
-
 
 alkeParking.showEarnings()
 
